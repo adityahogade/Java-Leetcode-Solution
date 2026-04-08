b7afe7537d90c3e2d4f71d7d1d82cb4af5fd62b3
Aditya hogade <adityahogade2612@gmail.com>
2026-04-08 21:34:43 +0530

    simple code

---
+++ Easy/src/Number_of_Good_Pairs.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+public class Number_of_Good_Pairs {
+    public int numIdenticalPairs(int[] nums) {
+        int n=nums.length;
+        HashMap<Integer,Integer> hm=new HashMap<>();
+        for(int i=0;i<n;i++){
+            hm.put(nums[i],hm.getOrDefault(nums[i],0)+1);
+
+        }
+        int ans=0;
+        for(int val: hm.keySet()){
+            int value=hm.get(val);
+            ans+= (value*(value-1))/2;
+        }
+        return ans;
+    }
+}
