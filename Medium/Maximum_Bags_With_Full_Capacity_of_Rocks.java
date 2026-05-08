7c59fb8ea454cbc9fbeba6b241bff1722912450a
Aditya hogade <adityahogade2612@gmail.com>
2026-05-08 09:48:12 +0530

    binary search

---
+++ Medium/Maximum_Bags_With_Full_Capacity_of_Rocks.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int maximumBags(int[] capacity, int[] rocks, int additionalRocks) {
+
+        int n = capacity.length;
+        int[] remaining = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            remaining[i] = capacity[i] - rocks[i];
+        }
+
+        Arrays.sort(remaining);
+
+        int count = 0;
+
+        for(int i = 0; i < n; i++) {
+
+            if(remaining[i] <= additionalRocks) {
+                additionalRocks -= remaining[i];
+                count++;
+            }
+            else {
+                break;
+            }
+        }
+
+        return count;
+    }
+}
